b17fed97804f708ea8b50ce7b586fcfd1f90f5a2
Olamide Okunola <olamideokunola@yahoo.com>
2022-04-03 01:10:55 +0100

    iframe metamask detection removed

--- cryptobankPayment.js
+++ cryptobankPayment.js
@@ -67,47 +67,5 @@ window.onmessage = function (event) {
         cryptoBankPopup.closeIframe()
         cryptoBankPopup?.onClose()
         console.log(event.origin)
-    } else if  (event.data.success && event.data.msg === 'detect-metamask') {
-        let detectedProvider = await reliableDetector().detect()
-        
-        cryptoBankPopup.iFrame.contentWindow.postMessage({
-            success: true,
-            msg: 'metamask-detection',
-            provider: detectedProvider
-        })
-
-    }
-}
-
-let reliableDetector = async () => {
-    let detect = () => {
-        
-        if (window.ethereum) {
-            return handleEthereum();
-        } else {
-            window.addEventListener('ethereum#initialized', handleEthereum, {
-            once: true,
-        });
-        
-            // If the event is not dispatched by the end of the timeout,
-            // the user probably doesn't have MetaMask installed.
-            setTimeout(handleEthereum, 3000); // 3 seconds
-        }
-        
-        function handleEthereum() {
-            const { ethereum } = window;
-            if (ethereum && ethereum.isMetaMask) {
-                console.log('Ethereum successfully detected!!!');
-                console.log(ethereum)
-                return ethereum
-                // Access the decentralized web!
-            } else {
-                console.log('Please install MetaMask!');
-            }
-        }
-    }
-
-    return {
-        detect
     }
 }
